6615709d33c4c264202c35736a0d109367f988a0
Ben2445Fun <benjamin.arent@gmail.com>
2024-10-08 11:03:53 -0600

    good

--- CSCI260/psqlintegration.py
+++ CSCI260/psqlintegration.py
@@ -3,7 +3,10 @@ database_in = input("Database: ")
 host_in = input("Host Address: ")
 username_in = input("Username: ")
 password_in = input("Password: ")
-connection = psycopg2.connect(database=database_in, host=host_in, user=username_in, password=password_in)
+connection = psycopg2.connect(database=database_in,
+                              host=host_in,
+                              user=username_in,
+                              password=password_in)
 cursor = connection.cursor()
 while True :
   print("What would you like to do?\n",
@@ -12,8 +15,10 @@ while True :
         "[3] Quit")
   selection = input("Enter: ")
   if   selection == "1" :
-    cursor.execute("\dt") #Doesn't like :/
-    print(*cursor)
+    cursor.execute("select table_schema, table_name from information_schema.tables where table_type = 'BASE TABLE' and table_schema not in ('information_schema','pg_catalog')")
+    tables = cursor.fetchall()
+    for table in tables:
+      print(table)
   elif selection == "2" :
     print("Selected 2")
   else :
