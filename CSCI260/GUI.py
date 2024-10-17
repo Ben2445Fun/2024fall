28932fc7211fcf0f6d2aff6a842a32e5e99f8cf1
Ben2445Fun <benjamin.arent@gmail.com>
2024-10-17 12:08:38 -0600

    So I can work more on my computer

--- CSCI260/GUI.py
+++ CSCI260/GUI.py
@@ -1,15 +1,14 @@
-#https://coderslegacy.com/python/python-gui/python-tkinter-frame/
-
 from tkinter import *
 from showCourses import ShowStr
 from addCourses import addCourse
 
-def addCourseClick(win,textCourse,textNumber):
-    dept=textCourse.get("1.0","end-1c")
+# Add course
+def addCourseClick(win,textCRN,textDepartment,textNumber):
+    crn = textCRN.get("1.0","end-1c")
+    dept=textDepartment.get("1.0","end-1c")
     number=textNumber.get("1.0","end-1c")
-    addCourse(dept,number)
+    addCourse(crn,dept,number)
     win.destroy()
-
 def addCourseWindow(root):
     win=Toplevel(root)
     win.title("Add a Course")
@@ -19,22 +18,73 @@ def addCourseWindow(root):
     
     rightframe = Frame(win)
     rightframe.pack(side=RIGHT)
-    labelCourse = Label(leftframe, text = "Course Department")
-    labelCourse.pack()
-    textCourse=Text(rightframe, height=1, width=30)
-    textCourse.pack()
+    labelCRN = Label(leftframe,text="CRN")
+    labelCRN.pack()
+    textCRN = Text(rightframe,height=1,width=30)
+    textCRN.pack()
+    labelDepartment = Label(leftframe, text = "Course Department")
+    labelDepartment.pack()
+    textDepartment=Text(rightframe, height=1, width=30)
+    textDepartment.pack()
     labelNumber = Label(leftframe, text = "Course Number")
     labelNumber.pack()
     textNumber=Text(rightframe, height=1, width=30)
     textNumber.pack()
     #add = Button(leftframe,command=addCourse,text = "Add the Course")
-    add = Button(rightframe,command=lambda: addCourseClick(win,textCourse,textNumber),text = "Add the Course")
+    add = Button(rightframe,command=lambda: addCourseClick(win,textCRN,textDepartment,textNumber),text = "Add the Course")
     add.pack(padx = 3, pady = 3)
     cancel=Button(leftframe,command=win.destroy,text="Cancel")
     cancel.pack(padx=3,pady=3)
     
     #grab_release()
 
+# Remove Course
+def removeCourseClick(win):
+    exit()
+def removeCourseWindow(root):
+    win=Toplevel(root)
+    leftframe = Frame(win)
+    leftframe.pack(side=LEFT)
+    rightframe = Frame(win)
+    rightframe.pack(side=RIGHT)
+    switchCRN = Button(leftframe,text="Course Reference Number",command=lambda:removeCoursebyCRN(leftframe,rightframe,win,switchCRN,switchDN))
+    switchCRN.pack()
+    switchDN = Button(rightframe,text="Department & Number",command=lambda:removeCoursebyDN(leftframe,rightframe,win,switchCRN,switchDN))
+    switchDN.pack()
+def removeCoursebyCRN(leftframe,rightframe,win,switchCRN,switchDN):
+    switchCRN.config(relief=SUNKEN,state=DISABLED)
+    switchDN.config(relief=RAISED,state=NORMAL)
+    labelCRN = Label(leftframe,text="CRN")
+    labelCRN.pack()
+    textCRN = Text(rightframe,height=1,width=30)
+    textCRN.pack()
+    removeCourseOptions(leftframe,rightframe,win)
+def removeCoursebyDN(leftframe,rightframe,win,switchCRN,switchDN):
+    switchCRN.config(relief=RAISED,state=NORMAL)
+    switchDN.config(relief=SUNKEN,state=DISABLED)
+    labelDepartment = Label(leftframe,text="Department")
+    labelDepartment.pack()
+    textDepartment = Text(rightframe,height=1,width=30)
+    textDepartment.pack()
+    labelNumber = Label(leftframe,text="Number")
+    labelNumber.pack()
+    textNumber = Text(rightframe,height=1,width=30)
+    textNumber.pack()
+    removeCourseOptions(leftframe,rightframe,win)
+    lambda: removeCourseOptions()
+def removeCourseOptions(leftframe,rightframe,win):
+    confirm = Button(leftframe,text="Delete Course")
+    confirm.pack()
+    cancel = Button(rightframe,text="Cancel",command=win.destroy)
+    cancel.pack()
+
+# Update Course
+def updateCourseClick(win):
+    exit()
+def updateCourseWindow(root):
+    exit()
+
+# Update course list
 def updateCourseList(event,text):
     global root
     if event.widget == root:
@@ -42,6 +92,8 @@ def updateCourseList(event,text):
         text.delete('1.0', END)
         text.insert(END, ShowStr())
 
+
+# Setup default window
 root = Tk()
 
 frame = Frame(root)
@@ -58,7 +110,7 @@ label.pack()
  
 add = Button(leftframe,command=lambda: addCourseWindow(root),text = "Add a Course")
 add.pack(padx = 3, pady = 3)
-remove = Button(leftframe, text = "Remove a Course")
+remove = Button(leftframe, text = "Remove a Course",command=lambda:removeCourseWindow(root))
 remove.pack(padx = 3, pady = 3)
 update = Button(leftframe, text = "Update a Course")
 update.pack(padx = 3, pady = 3)
@@ -66,6 +118,6 @@ update.pack(padx = 3, pady = 3)
 text=Text(rightframe, height=20, width=30)
 text.pack()
 
-root.title("Cool Course Editor")
+root.title("Course Editor")
 root.bind("<FocusIn>", lambda event: updateCourseList(event,text))
 root.mainloop()
--- CSCI260/addCourses.py
+++ CSCI260/addCourses.py
@@ -7,19 +7,20 @@
 
 from db import *
 
-def addCourse(dept,number):
+def addCourse(crn,dept,number):
     cursor=connectDB()
     #Use Fstring Formatted String to add class and number to query
-    query="insert into courses (class,number) values ('%s','%s');" %(dept,number)
+    query="insert into courses (crn,class,number) values ('%s','%s','%s');" %(crn,dept,number)
     print(query)
     cursor.execute(query)
     disconnectDB()
 
 def Add():
     print("Adding a class to the offered classes")
+    crn=input("Please enter the class reference number:")
     classDept=input("Please enter the class Department:")
     classNum=input("Please enter the class Number:")
-    addCourse(classDept,classNum)
+    addCourse(crn,classDept,classNum)
 
 if __name__ == "__main__":
     Add()
