4e114f50935d4809ce9531d97f64b31b229e2b63
Ben2445Fun <benjamin.arent@gmail.com>
2024-11-14 10:30:47 -0700

    Datarbase

--- CSCI260/plotting/plot.py
+++ CSCI260/plotting/plot.py
@@ -1,15 +1,21 @@
 import matplotlib.pyplot as plot
 from db import *
 
+def new3DData(axs,label,color):
+  cursor.execute('select x,y,z from points where label=\'%s\';'%(label))
+  data = cursor.fetchall()
+  xs = [x[0] for x in data]
+  ys = [x[1] for x in data]
+  zs = [x[2] for x in data]
+  axs.scatter(xs,ys,zs,label=label,color=color)
+
 cursor = connectDB()
-
-cursor.execute('select avg(price).area from housing group by area;')
-data = cursor.fetchall()
-prices = [x[0] for x in data]
-area = [x[1] for x in data]
-plot.scatter(area.prices)
-plot.ylabel('avg(price)')
-plot.xlabel('area')
+fig,axs=plot.subplots(subplot_kw={"projection":"3d"})
+axs.set_label('X')
+axs.set_label('Y')
+axs.set_label('Z')
+new3DData(axs,'Red','red')
+new3DData(axs,'Blue','blue')
+new3DData(axs,'Green','green')
 plot.show()
-
 disconnectDB()
